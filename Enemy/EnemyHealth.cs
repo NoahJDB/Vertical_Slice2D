4c9ccfbe4d203bdb8589d4dfb6a3353d8111749f
NJDB <23075@ma-web.nl>
2018-02-05 15:43:41 +0100

    code

---
+++ Enemy/EnemyHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EnemyHealth : MonoBehaviour
+{
+   // private AudioScript _audio;
+    private int fallDamage;
+   
+
+  
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+        if (Mathf.Abs(other.relativeVelocity.y) > 6f)
+        {
+            Destroy(this.gameObject);
+            
+        }
+    }
+}
